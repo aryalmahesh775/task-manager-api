31d26a20241fa958c4ac81f99095a8158a9f9446
aryalmahesh775 <aryalmahesh775@gmail.com>
2020-04-15 06:36:34 +0545

    Starting the project 2077-1-03

---
+++ src/db/mongoose.js
@@ -0,0 +1,51 @@
+const mongoose = require('mongoose')
+
+mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+})
+
+const User = mongoose.model('User', {
+  name: {
+    type: String,
+  },
+  age: {
+    type: String,
+  },
+})
+
+// const me = new User({
+//   name: 'Mahesh ',
+//   age: 27,
+// })
+
+// me.save()
+//   .then(() => {
+//     console.log(me)
+//   })
+//   .catch((error) => {
+//     console.log('Error!', error)
+//   })
+
+const Task = mongoose.model('Task', {
+  description: {
+    type: String,
+  },
+  completed: {
+    type: Boolean,
+  },
+})
+
+const task = new Task({
+  description: 'Learn the mongoose library',
+  completed: false,
+})
+
+task
+  .save()
+  .then(() => {
+    console.log(task)
+  })
+  .catch(() => {
+    console.log(error)
+  })
