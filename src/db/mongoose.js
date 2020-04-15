46c6d569bb4d52e5a3c66ee045d7942ff065db25
aryalmahesh775 <aryalmahesh775@gmail.com>
2020-04-15 12:18:30 +0545

    Reading the files in mongo

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -5,28 +5,3 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
   useCreateIndex: true,
   useUnifiedTopology: true,
 })
-
-const Task = mongoose.model('Task', {
-  description: {
-    type: String,
-    required: true,
-    trim: true,
-  },
-  completed: {
-    type: Boolean,
-    default: false,
-  },
-})
-
-const task = new Task({
-  description: 'Eat Lunch',
-})
-
-task
-  .save()
-  .then(() => {
-    console.log(task)
-  })
-  .catch((error) => {
-    console.log(error)
-  })
