139ded1e4231b3346719ca736e795bd7f833a3df
aryalmahesh775 <aryalmahesh775@gmail.com>
2020-04-18 12:40:53 +0545

    Auth and security

--- src/index.js
+++ src/index.js
@@ -6,6 +6,14 @@ const taskRouter = require('./routers/task')
 const app = express()
 const port = process.env.PORT || 3000
 
+// app.use((req, res, next) => {
+//   if (req.method === 'GET') {
+//     res.send('GET request are disabled')
+//   } else {
+//     next()
+//   }
+// })
+
 app.use(express.json())
 app.use(userRouter)
 app.use(taskRouter)
@@ -13,3 +21,18 @@ app.use(taskRouter)
 app.listen(port, () => {
   console.log('Server is up on port ' + port)
 })
+
+const Task = require('./models/task')
+const User = require('./models/user')
+
+const main = async () => {
+  // const task = await Task.findById('5e99ec0ae3021a05f47b66c8')
+  // await task.populate('owner').execPopulate()
+  // console.log(task.owner)
+
+  const user = await User.findById('5e99ea8f0378fd45dc93eecd')
+  await user.populate('tasks').execPopulate()
+  console.log(user.tasks)
+}
+
+main()
